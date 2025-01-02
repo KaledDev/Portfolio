524b2ea1b77c7e64861c14b9cb565cb9a819bb83
KaledDev <sanguissotommy22@gmail.com>
2025-01-02 19:31:07 +0000

    Ajout de Vercel analytics

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,11 @@
 <script setup>
 import NavBar from '@/components/NavBar.vue';
 import { RouterView } from 'vue-router';
+import { Analytics } from '@vercel/analytics/vue';
 </script>
 
 <template>
+<Analytics/>
 <NavBar/>
 <RouterView/>
 </template>
