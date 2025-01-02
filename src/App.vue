331c448b6a4555247daf0c18661b7c075e3031dc
KaledDev <sanguissotommy22@gmail.com>
2025-01-02 19:36:07 +0000

    Ajoutr de Speed Insights

--- src/App.vue
+++ src/App.vue
@@ -2,9 +2,11 @@
 import NavBar from '@/components/NavBar.vue';
 import { RouterView } from 'vue-router';
 import { Analytics } from '@vercel/analytics/vue';
+import { SpeedInsights } from '@vercel/speed-insights/vue';
 </script>
 
 <template>
+<SpeedInsights/>
 <Analytics/>
 <NavBar/>
 <RouterView/>
